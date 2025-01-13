f1a379f6e959b77b8b52d2cb9d1c84e196dd5bc9
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-01-14 00:19:23 +0200

    Display working

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-const button = document.getElementsByClassName
+const display = document.querySelector('input');
+const buttons = document.querySelectorAll('button');
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-ops]');
+const equals = document.querySelector('[data-equal]');
+const clear = document.querySelector('[data-clear]');
+
+
+function zero() {
+    display.value = 0;
+};
+
 
 function operate() {
 
 }
+
+zero();
