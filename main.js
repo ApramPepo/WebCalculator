fd5f1511d4acce43bd2dfeb8c809380ada42857d
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-01-16 16:17:20 +0200

    code functions added

--- main.js
+++ main.js
@@ -4,14 +4,35 @@ const numbers = document.querySelectorAll('[data-number]');
 const operators = document.querySelectorAll('[data-ops]');
 const equals = document.querySelector('[data-equal]');
 const clear = document.querySelector('[data-clear]');
+const backspace = document.querySelector('[data-bckspce]');
 
 
 function zero() {
     display.value = 0;
 };
 
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+};
 
-function operate() {
+function sub(num1, num2) {
+    return Number(num1) - Number(num2);
+};
+
+function mult(num1, num2) {
+    return Number(num1) * Number(num2);
+};
+
+function div(num1, num2) {
+    if (num1 == 0 || num2 == 0) {
+        return "ERROR";
+    }
+    return Number(num1) / Number(num2);
+};
+
+
+
+function operate(num1, op, num2) {
 
 }
 
