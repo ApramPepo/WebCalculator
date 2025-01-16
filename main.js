c9dbefc8d2142d96cbdae4201e5e1a8a3da39d7f
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2025-01-16 20:09:45 +0200

    somewhat functioning Calculator

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const numbers = document.querySelectorAll('[data-number]');
 const operators = document.querySelectorAll('[data-ops]');
 const equals = document.querySelector('[data-equal]');
 const clear = document.querySelector('[data-clear]');
-const backspace = document.querySelector('[data-bckspce]');
+const backspce = document.querySelector('[data-bckspce]');
+
+let num1 = '';
+let num2 = '';
+let oper = '';
+let decimal = false;
 
 
 function zero() {
@@ -32,8 +37,91 @@ function div(num1, num2) {
 
 
 
-function operate(num1, op, num2) {
-
+function operate(num1, oper, num2) {
+    switch (oper) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return sub(num1, num2);
+        case '*':
+            return mult(num1, num2);
+        case '/':
+            return div(num1, num2);
+        default:
+            break;
+    }
 }
 
-zero();
+function setNumbers(num) {
+    if (oper === '') {
+        num1 += num;
+    }
+    else {
+        num2 += num;
+    }
+}
+
+function checkOper() {
+    if (!oper) {
+        return;
+    }
+    return false;
+}
+
+function setOper(operator) {
+    if (num1 && !oper) {
+        oper = operator;
+    }
+    decimal = false;
+}
+function check() {
+    if (oper && num1 && num2) {
+        return true;
+    }
+    return false;
+}
+
+function checkDecimal() {
+    if (!decimal && !op) {
+        num1 += '.';
+        display.value = num1;
+        decimal = true;
+    }
+    else if (!decimal) {
+        num2 += '.';
+        display.value = num2;
+        decimal = true;
+    }
+}
+
+function backspace() {
+    if (!oper && num1) {
+        display.value = display.value.slice(0, -1);
+        num1 = num1.slice(0, -1);
+    }
+    else if (oper && !num2) {
+        display.value = display.value.slice(0, -1);
+        oper = "";
+    }
+    else if (oper && num2) {
+        display.value = display.value.slice(0, -1);
+        num2 = num2.slice(0, -1);
+    }
+}
+
+clear.addEventListener('click', () => {
+    zero();
+    num1 = '';
+    num2 = '';
+    oper = '';
+    decimal = false;
+});
+
+backspce.addEventListener('click', () => {
+    backspace();
+});
+
+let nine = document.querySelector('.btn-nine');
+nine.addEventListener('click, () => {
+    display.value += 9;
+});
